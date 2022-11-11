f6bb68681d353fed3b47ad40e6882cd72f94038d
KwameSei <nathanielosei92@gmail.com>
2022-11-11 19:41:25 +0000

    Custom error pages created

--- app.py
+++ app.py
@@ -11,3 +11,14 @@ def home():
 @app.route('/user/<name>')
 def user(name):
     return render_template("user.html", user_name=name)
+
+#Creating custom error pages
+#Invalid URL
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("not_found.html"), 404
+
+#Internal Server Error
+@app.errorhandler(404)
+def page_500(e):
+    return render_template("error_500.html"), 500
