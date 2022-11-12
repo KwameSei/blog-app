3aab9dddca1320ca27159ebefe7d269a768bf289
KwameSei <nathanielosei92@gmail.com>
2022-11-12 14:35:08 +0000

    static files templates created

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, flash
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 #Create flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'Secret key no one is supposed to know'
+
+#Creating a form class
+class nameForm(FlaskForm):
+    name = StringField("What's your name?", validators=[DataRequired()])
+    submit = SubmitField("Submit")
 
 #Create a route decorator
 @app.route('/')
@@ -11,6 +20,17 @@ def home():
 @app.route('/user/<name>')
 def user(name):
     return render_template("user.html", user_name=name)
+
+@app.route('/name', methods=['GET', 'POST']) 
+def name():
+    name = None
+    form = nameForm()
+    # Validate form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        flash("Form Submitted Successfully")
+    return render_template("name.html", name = name, form=form)
 
 #Creating custom error pages
 #Invalid URL
